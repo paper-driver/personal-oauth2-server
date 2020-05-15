b6dfa99b5f9c3d1eeba4cca8c8d14df16e87cdc7
paper-driver <yuxuan.mao.1@gmail.com>
2020-05-15 17:35:01 -0400

    first empty project

---
+++ src/main/java/com/paperdriver/personalauthserver/PersonalAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.paperdriver.personalauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paperdriver/personalauthserver/PersonalAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperdriver.personalauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
